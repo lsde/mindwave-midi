47d4a5479710bb199f3beaa35747d7b83a9416b2
Jonas Svatos <lsde@lsde.org>
2019-11-19 13:04:48 +0000

    tweaks

--- mindwave-midi.py
+++ mindwave-midi.py
@@ -22,22 +22,21 @@ if available_ports:
 if (mindwaveDataPointReader.isConnected()):    
     with midiout:
         while(True):
-            dataPoint = mindwaveDataPointReader.readNextDataPoint()
+            for i in range(0,7):
+                dataPoint = mindwaveDataPointReader.readNextDataPoint()
             if (dataPoint.__class__ is RawDataPoint):
                 omindwave_value = int(dataPoint._readRawValue())
 
                 # Rescale the value to match MIDI spec
                 # https://stackoverflow.com/questions/929103/convert-a-number-range-to-another-range-maintaining-ratio
-                mindwave_value = int((((omindwave_value - -2048) * (127 - 0)) / (2048 - -2048)) + 0)
-                print('RAW VALUE: {0}, MIDI VALUE: {1}'.format(omindwave_value, mindwave_value))
+                mindwave_value = int((((omindwave_value - -2048) * (15 - 0)) / (2048 - -2048)) + 0) * 8
 
-                note_on = [0x90, mindwave_value, 112]
-                note_off = [0x80, mindwave_value, 0]
-
-                midiout.send_message(note_on)
-                #time.sleep(0.05)
-                midiout.send_message(note_off)
-                #time.sleep(0.05)
+                if mindwave_value > 64 or mindwave_value < 40:
+                    print('RAW VALUE: {0}, MIDI VALUE: {1}'.format(omindwave_value, mindwave_value))
+                    note_on = [0x90, mindwave_value, 112]
+                    note_off = [0x80, mindwave_value, 0]
+                    midiout.send_message(note_on)
+                    midiout.send_message(note_off)
     del midiout
 else:
     print('Error connecting to MindWave Mobile!')
